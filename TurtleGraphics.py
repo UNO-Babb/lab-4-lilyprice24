cd22128c7e439fdc588c048e35d5e81d0260bab6
lilyprice24 <lilyprice@unomaha.edu>
2024-09-20 13:24:14 -0500

    Updated TurtleGraphics.py

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -44,15 +44,13 @@ def squaresInSquares(myTurtle, num):
     for i in range(num):
         drawSquare(myTurtle, size)
         size = size + 20
+        myTurtle.penup()
         myTurtle.right(180)
         myTurtle.forward(10)
         myTurtle.right(90)
         myTurtle.forward(10)
         myTurtle.right(90)
-
-
-
-
+        myTurtle.pendown()
 
 
 
@@ -63,7 +61,7 @@ def main():
     # drawPolygon(myTurtle, 8) #draws an octogon
 
     #fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
+    #fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
 
     squaresInSquares(myTurtle, 5) #draws 5 concentric squares
     # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
